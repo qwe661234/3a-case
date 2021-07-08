053b7fe1807d6512a91a3c653b630810896f4672
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:32:09 +0800

    feat: add container TestListTableContiner and component TestListTable

---
+++ src/components/TestListTableContainer/TestListTable.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-table :data="tabledata" height="250" border style="width: 100%">
+        <el-table-column prop="img" label="" width="180"> </el-table-column>
+        <el-table-column prop="title" label="名稱" width="180">
+        </el-table-column>
+        <el-table-column prop="socre" label="及格分數"> </el-table-column>
+        <el-table-column prop="difficulty" label="難易度"> </el-table-column>
+        <el-table-column prop="status" label="活動狀態"> </el-table-column>
+        <el-table-column fixed="right" label="操作" width="120">
+            <template slot-scope="scope">
+                <el-button
+                    type="text"
+                    size="small"
+                    @click="handleEdit(scope.$index, scope.row)"
+                >
+                    編輯內容
+                    <i class="el-icon-edit"></i>
+                </el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+export default {
+    name: "TestListTable",
+    props: {
+        tabledata: {
+            type: Array,
+            default() {
+                return [
+                    {
+                        img: "",
+                        title: "",
+                        socre: "",
+                        difficulty: "",
+                        status: "",
+                    },
+                ];
+            },
+        },
+    },
+    methods: {
+        handleEdit(index, row) {
+            console.log(index, row);
+        },
+    },
+};
+</script>
---
+++ src/container/TestList/TestListTableContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-row>
+        <Table :tabledata="TestData" />
+    </el-row>
+</template>
+
+<script>
+import Table from "@/components/TestListTableContainer/TestListTable";
+import { mockTest } from "@/mock/test.js";
+export default {
+    name: "TestListTableContainer",
+    components: { Table },
+    data() {
+        return {
+            TestData: mockTest,
+        };
+    },
+};
+</script>
