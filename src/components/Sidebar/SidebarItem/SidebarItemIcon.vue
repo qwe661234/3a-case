1948b312ff905cdc42e5f093680c6ebb6befd3de
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:28:39 +0800

    feat: add components in SideBarItem
    1. SideBarItemIcon
    2. SideBarItemText

---
+++ src/components/Sidebar/SidebarItem/SidebarItemIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <i :class="icon"></i>
+</template>
+
+<script>
+export default {
+    name: "SidebarItemIcon",
+    props: {
+        icon: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
---
+++ src/components/Sidebar/SidebarItem/SidebarItemText.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>{{ text }}</div>
+</template>
+
+<script>
+export default {
+    name: "SidebarTitleText",
+    props: {
+        text: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/color.scss";
+div {
+    color: $color-lightGray;
+    text-align: left;
+}
+</style>
