cd1be6430fa714dafc93f4806b4f6480823ddd09
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:30:38 +0800

    feat: add components in TestListTopbar
    1. TestListTopbarAddTestButton
    2. TestListTopbarSearchInput

---
+++ src/components/TestListTopbar/TestListTopbarAddtestButton.vue
@@ -0,0 +1,3 @@
+<template>
+    <el-button type="primary" icon="el-icon-plus">建立考試</el-button>
+</template>
---
+++ src/components/TestListTopbar/TestListTopbarSearchInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <el-input placeholder="輸入考試標題" suffix-icon="el-icon-search">
+    </el-input>
+</template>
+
+<style lang="scss" scoped>
+.el-input {
+    width: 209px;
+    height: 33px;
+    box-shadow: 2px 3px 3px 3px #cccccc;
+    border-radius: 30px;
+}
+</style>
