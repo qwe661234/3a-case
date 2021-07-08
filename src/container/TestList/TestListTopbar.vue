20bbd9e1d9af0fcc96a0afd960b87eb138583abe
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:31:02 +0800

    feat: add container TestListTopbar

---
+++ src/container/TestList/TestListTopbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-row>
+        <el-col :span="12">
+            <AddtestButton />
+        </el-col>
+        <el-col :span="12">
+            <SearchInput />
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+import AddtestButton from "@/components/TestListTopbar/TestListTopbarAddtestButton";
+import SearchInput from "@/components/TestListTopbar/TestListTopbarSearchInput";
+export default {
+    name: "Topbar",
+    components: {
+        AddtestButton,
+        SearchInput,
+    },
+};
+</script>
