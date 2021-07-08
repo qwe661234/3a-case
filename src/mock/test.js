dc7f5943a0fcf8b1c5c021659de1cd3de9dd32a4
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:32:42 +0800

    feat: add mock data used in TestListTable

---
+++ src/mock/test.js
@@ -0,0 +1,16 @@
+export const mockTest = [
+    {
+        img: "a",
+        title: "標題",
+        socre: "ＸＸ分",
+        difficulty: "簡易",
+        status: "上架",
+    },
+    {
+        img: "a",
+        title: "標題",
+        socre: "ＸＸ分",
+        difficulty: "困難",
+        status: "未上架",
+    },
+];
