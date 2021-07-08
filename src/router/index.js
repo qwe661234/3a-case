35062efef03ba41242a4c75481656f4213e7a22b
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:43:21 +0800

    fix: modify router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
-// import App from "../App.vue";
+import App from "../App.vue";
 import TestList from "../pages/TestList";
 import BuildTest from "../pages/BuildTest";
 Vue.use(Router);
 
 export default new Router({
     routes: [
+        {
+            path: "/",
+            name: "App",
+            component: App,
+        },
         {
             path: "/TestList",
             name: "TestList",
