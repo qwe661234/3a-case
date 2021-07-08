fbf0660443c441e880c9cf33e77b0b5d3f204bac
Yen Fu Chen <qwe661234@gmail.com>
2021-07-08 12:27:28 +0800

    feat: add component NavbarTitle

---
+++ src/components/Navbar/NavbarTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h1">{{ text }}</div>
+</template>
+
+<script>
+export default {
+    name: "NavbarTitle",
+    props: {
+        text: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/color.scss";
+div {
+    color: $color-white;
+}
+</style>
